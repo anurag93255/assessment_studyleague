3a500604d70ba49b99d33bff713481066f613bae
anurag93255 <89122510+anurag93255@users.noreply.github.com>
2021-09-30 10:19:15 +0530

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,35 +1,38 @@
 <?php
 include('userModal.php');
+if(!isset($_POST['name']) || !isset($_POST['email'])) {
+    header("Location: http://{$_SERVER['SERVER_NAME']}/assessment/add_user_form.php");die;
+}
 $name = $_POST['name'];
 $email = $_POST['email'];
 
 
+
 $user = new user();
 $total_user = (mysqli_fetch_array($user->getTotalUser())['total'] + 1);
-
-
+// echo $total_user;die;
 switch(true) {
     case ($total_user > 14):
-        $temp = ($total_user - 14 / 8);
+        $temp = (($total_user - 14) / 8);
         
         // Here we use block as user_id
-        $block_id = ($temp == ceil($temp) ? ($temp + 2) : ceil($temp) + 1 );
+        $block_id = ($temp == ceil($temp) ? ($temp + 2) : (ceil($temp) + 1) );
         
-        if(fmod($total_user-14, 8) == 0) {
+        if(fmod(($total_user-14), 8) == 0) {
             $user->incrementPoleByOne($block_id);
             // $user->incrementAmountByOne(1);
-            if(fmod($total_user, 13*8) == 0) {
+            if(fmod($total_user, (13*8)) == 0) {
                 $user->incrementPoleByOne(1);
                 $user->incrementAmountByOne(1);
             }
         }
         $user->incrementAmountByOne($block_id);
         break;
+    case ($total_user <= 14):
+        $user->incrementAmountByOne(1);
     case ($total_user == 14):
         $user->incrementPoleByOne(1);
-        break;
-    case ($total_user < 14):
-        $user->incrementAmountByOne(1);
+    
 }
 
 $insertion_arr = [
@@ -39,4 +42,5 @@ $insertion_arr = [
     'amount' => 1
 ];
 $result = $user->insertUser($insertion_arr);
+// echo $block_id;
 header("Location: http://{$_SERVER['SERVER_NAME']}/assessment/add_user_form.php");
